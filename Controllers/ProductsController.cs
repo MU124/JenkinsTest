deff57bab7aa11f4ce4c8264da19394d81d2469e
Mayur Utekar <mayurutekar124@gmail.com>
2024-08-26 17:35:41 +0530

    INIT

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using CURD.Models;
+using CURD.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CURD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return ProductRepository.GetAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = ProductRepository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            ProductRepository.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingProduct = ProductRepository.Get(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            ProductRepository.Update(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = ProductRepository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ProductRepository.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using CURD.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CURD.Repositories
+{
+    public class ProductRepository
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.0M },
+            new Product { Id = 2, Name = "Product 2", Price = 20.0M }
+        };
+
+
+        public static IEnumerable<Product> GetAll() => Products;
+
+        public static Product Get(int id) => Products.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Product product) => Products.Add(product);
+
+        public static void Update(Product product)
+        {
+            var index = Products.FindIndex(p => p.Id == product.Id);
+            if (index != -1)
+            {
+                Products[index] = product;
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            var product = Get(id);
+            if (product != null)
+            {
+                Products.Remove(product);
+            }
+        }
+
+    }
+}
