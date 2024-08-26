ff2da8aff2d0c211595839c0d09229ac3c016468
Mayur Utekar <mayurutekar124@gmail.com>
2024-08-26 18:05:27 +0530

    Added : 3rd project

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -9,7 +9,8 @@ namespace CURD.Repositories
         private static List<Product> Products = new List<Product>
         {
             new Product { Id = 1, Name = "Product 1", Price = 10.0M },
-            new Product { Id = 2, Name = "Product 2", Price = 20.0M }
+            new Product { Id = 2, Name = "Product 2", Price = 20.0M },
+            new Product { Id = 3, Name = "Product 3", Price = 30.0M }
         };
 
 
