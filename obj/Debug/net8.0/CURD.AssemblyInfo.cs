1a752c1e6b3d6f02dd4e46651d96cf2f0fc5a634
Mayur Utekar <mayurutekar124@gmail.com>
2024-08-26 18:30:04 +0530

    TTS

--- obj/Debug/net8.0/CURD.AssemblyInfo.cs
+++ obj/Debug/net8.0/CURD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CURD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deff57bab7aa11f4ce4c8264da19394d81d2469e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2da8aff2d0c211595839c0d09229ac3c016468")]
 [assembly: System.Reflection.AssemblyProductAttribute("CURD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CURD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/CURD.AssemblyInfo.cs
+++ obj/Release/net8.0/CURD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CURD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2da8aff2d0c211595839c0d09229ac3c016468")]
 [assembly: System.Reflection.AssemblyProductAttribute("CURD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CURD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
